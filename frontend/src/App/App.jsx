1da2a7ba0b53f2c4b1c1e5ae5219e85ef44f72bd
Elton <mesquita2019e.l.t.o.n@gmail.com>
2026-05-10 12:52:06 +0200

    tela principal

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -7,11 +7,14 @@ import SolicitacoesVendedor from '../pages/Vendedor/SolicitacoesVendedor';
 import SolicitacoesIntermediario from '../pages/Intermediario/SolicitacoesIntermediario';
 import SolicitacoesCliente from '../pages/Cliente/SolicitacoesCliente';
 import TelaChat from '../pages/Chat/TelaChat';
+import TelaPrincipal from '../pages/Principal/TelaPrincipal';
 function App() {
   return (
-    <Routes>
+    <Routes> 
       <Route path="/" element={<AuthPage />} />
       <Route path="/auth" element={<AuthPage />} />
+      {/* PRINCIPAL */}
+      <Route path="/principal" element={<TelaPrincipal />} />
       {/* CHAT */}
       <Route path="/chat" element={<TelaChat />} />
       {/* VENDEDOR */}
